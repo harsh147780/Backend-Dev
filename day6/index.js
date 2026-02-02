a42dfc96a910faf876755d0f1445f425597fc8b1
Harsh <harshchaudhary1477@gmail.com>
2026-02-02 14:16:06 +0530

    day 6 ExpressJs

---
+++ day6/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app =express();
+const port=8000;
+
+const student=[
+    {id:1,name:"shubham",branch:"cse"},
+    {id:2,name:"batsal",branch:"ece"},
+    {id:3,name:"akh1kyro",branch:"diploma"}
+]
+
+app.get("/",(req,res)=>{
+    res.send("welcome to home page")
+})
+
+app.get("/user",(req,res)=>{
+    res.send("user")
+})
+
+app.get("/student",(req,res)=>{
+    res.json(student)
+})
+
+app.get("/student/:id",(req,res)=>{
+    res.send(" ")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
